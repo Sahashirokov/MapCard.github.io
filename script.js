f84d3dc6185f3ce6dcfb6659fec0dcc83814ffc0
Sasha Shirokov <Sahca.Shirokov.69@gmail.com>
2024-06-19 19:08:54 +0500

    delete touch and add button

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 ymaps.ready(init);
 
+function openCard(event)
+{
+    const btn = event.target;
+    const currentcard = btn.closest('.Card');
+    const currentitem = currentcard.querySelector('.item3');
+    currentitem.style.height=`35vh`;
+
+}
+
 function init() {
     var myMap = new ymaps.Map("map", {
             center: [54.73517666496014,55.95874577453613],
@@ -17,85 +26,57 @@ for (let i = 0; i < 3; i++)
 {
     myMap.geoObjects.add(placemarks[i]);
 }
-let startY = null;
-
-  const cards = document.querySelectorAll(".card");
-  const text = document.querySelector(".text");
-
-    cards.forEach(function (card)
-    {
-            card.addEventListener('touchstart',(event)=>{
-        const firstTouch = event.touches[0].clientY;
-        startY = firstTouch;
-    });
-          card.addEventListener('touchmove', (event) => {
-        let MoveY = event.touches[0].clientY;
-        if(startY>MoveY && text.style.height != `100%`)
-            {
-                if(startY-MoveY > 20)
-                    {
-                        card.style.height =`60vh`;
-                        card.style.bottom =`0px`;
-                        card.style.transform =`none`;
-                        text.style.height=`100%`;
-                    }
-                 else
-                 {
-                    card.style.transform =`translate(0px, -${startY-MoveY}px)`;
-                 }
-            }
-        if(text.style.height == `100%`)
-            {
-                if(startY-MoveY < 70)
-                    {
-                        card.style.height =`30vh`;
-                        card.style.bottom =`0px`;
-                        card.style.transform =`none`;
-                        text.style.height=`70%`;
-                    }
-            }
-
-
-});
-
-    });
-
-
 
 
 placemark.events.add('click', function () {
 
-    if(document.getElementsByClassName("card")[1].style.display=='block' || document.getElementsByClassName("card")[2].style.display=='block')
+cardOpen();
+
+
+    if(document.getElementsByClassName("Card")[1].style.display=='grid' || document.getElementsByClassName("Card")[2].style.display=='grid')
         {
-            document.getElementsByClassName("card")[1].style.display='none';
-            document.getElementsByClassName("card")[2].style.display='none';
-            document.getElementsByClassName("card")[0].style.display='block';
+            document.getElementsByClassName("Card")[1].style.display='none';
+            document.getElementsByClassName("Card")[2].style.display='none';
+            document.getElementsByClassName("Card")[0].style.display='grid';
         }
-        document.getElementsByClassName("card")[0].style.display='block';
+        document.getElementsByClassName("Card")[0].style.display='grid';
 
 });
 
     placemark1.events.add('click', function () {
 
-
-       if(document.getElementsByClassName("card")[0].style.display=='block' || document.getElementsByClassName("card")[2].style.display=='block')
+cardOpen();
+       if(document.getElementsByClassName("Card")[0].style.display=='grid' || document.getElementsByClassName("Card")[2].style.display=='grid')
         {
-            document.getElementsByClassName("card")[0].style.display='none';
-            document.getElementsByClassName("card")[2].style.display='none';
-            document.getElementsByClassName("card")[1].style.display='block';
+            document.getElementsByClassName("Card")[0].style.display='none';
+            document.getElementsByClassName("Card")[2].style.display='none';
+            document.getElementsByClassName("Card")[1].style.display='grid';
         }
-         document.getElementsByClassName("card")[1].style.display='block';
+         document.getElementsByClassName("Card")[1].style.display='grid';
 });
     placemark2.events.add('click', function () {
-    if(document.getElementsByClassName("card")[0].style.display=='block' || document.getElementsByClassName("card")[1].style.display=='block')
+        cardOpen();
+    if(document.getElementsByClassName("Card")[0].style.display=='grid' || document.getElementsByClassName("Card")[1].style.display=='grid')
         {
-            document.getElementsByClassName("card")[0].style.display='none';
-            document.getElementsByClassName("card")[1].style.display='none';
-            document.getElementsByClassName("card")[2].style.display='block';
+            document.getElementsByClassName("Card")[0].style.display='none';
+            document.getElementsByClassName("Card")[1].style.display='none';
+            document.getElementsByClassName("Card")[2].style.display='grid';
         }
-         document.getElementsByClassName("card")[2].style.display='block';
+         document.getElementsByClassName("Card")[2].style.display='grid';
 });
 
+}
+function cardOpen()
+{
+    const Cards = document.querySelectorAll(".Card");
+    Cards.forEach(function (card)
+    {
+         if(card.style.display=='none')
+             {
+                 let citem = card.querySelector('.item3');
+                 citem.style.height=`15vh`;
+             }
+    });
 }
 var tg = window.Telegram.WebApp;
 tg.expand();
